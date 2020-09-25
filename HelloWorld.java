3f07dc09c537f5ee630d1e7e2c558da5641e7794
Roberto Ventura <rventura1@njcu.edu>
2020-09-25 01:22:18 +0000

    this is my second git commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("My first java program on Linux");
+		System.out.println("My second commit on Linux");
 	}
 }
